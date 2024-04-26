5a94113eb9937108a7d12725386c4d7a885f2c2d
Shanti Swaroop Swain <63071443+swainsh@users.noreply.github.com>
2024-04-26 18:02:34 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import time, os
 from datetime import datetime
 
-with open('log.txt', 'w') as writer:
+with open(os.path.join(os.path.abspath(__FILE__), 'log.txt'), 'w') as writer:
   while True:
     writer.write(f"Hello {os.getlogin()}! {datetime.now()}")
     time.sleep(5)
