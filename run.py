4c6bc8ed9bb018245b44ef808ff56b30e8e6dbea
Shanti Swaroop Swain <63071443+swainsh@users.noreply.github.com>
2024-04-26 18:54:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import time, os, pwd
 from datetime import datetime
 
 while True:
-  with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log.txt'), 'w+') as writer:
+  with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log.txt'), 'a+') as writer:
     writer.write(f"Hello {pwd.getpwuid(os.getuid())[0]}! {datetime.now()}")
+  print(f"Hello {pwd.getpwuid(os.getuid())[0]}! {datetime.now()}")
   time.sleep(5)
